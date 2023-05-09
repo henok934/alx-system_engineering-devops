38c45944d4f29f0e4d3c801ef8c42635ed3e86de
teklemariam mossie <teklemariammossie1@gmail.com>
2023-05-09 05:56:47 -0700

    reddi api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    t = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
+    subs = t.get("data", {}).get("subscribers", 0)
     return subs
